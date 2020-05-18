86f0eda580a89a4c2fc703992c8329183e604575
PaQPaweł <paq-pawel1996@o2.pl>
2020-05-18 23:47:32 +0200

    Zadanie40

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var take = document.getElementById("result");
+console.log(take.value);
+function TakeData()
+{
+    document.querySelector("#name").value=document.querySelector("#fname");
+    document.querySelector("#surname").value=document.querySelector("#lname");
+    document.querySelector("#phone").value=document.querySelector("#telephone");
+}
